1581828f6a2a5a0b41e3023cafa0c0d5b8a9d8c6
Andy <tekawadeaniket9909@gmail.com>
2022-09-27 15:48:23 +0530

    second time commited

--- color-game.js
+++ color-game.js
@@ -1,11 +1,4 @@
-// var colors = [
-//   "rgb(255, 0, 255)",
-//   "rgb(0, 0, 255)",
-//   "rgb(255, 0, 0)",
-//   "rgb(0, 255, 0)",
-//   "rgb(0, 255, 255)",
-//   "rgb(255, 255, 0)"
-// ];
+
 var colors = ganaratcolor(6);
 var squares=document.querySelectorAll(".square");
 var pickedcolor=colors[Math.floor(Math.random() * colors.length)];
@@ -13,8 +6,8 @@ var colordispla=document.getElementById("colordisplay");
 var result =document.getElementById("result");
 var head1 = document.querySelector("h1");
 var newgame=document.getElementById("newgame");
-var hardlevel = document.getElementsByClassName("btn1");
-var easylevel = document.getElementsByClassName("btn2");
+var hardlevel = document.querySelector(".btn1");
+var easylevel = document.querySelector(".btn2");
 var eayssquare=document.querySelectorAll("#easysquare");
 
 colordispla.innerText=pickedcolor;
@@ -26,24 +19,21 @@ colordispla.innerText=pickedcolor;
     squares[i].addEventListener("click",function(){
       if (this.style.backgroundColor === pickedcolor) {
         // alert("right");
-        result.textContent="right";
+        result.src="correct.png";
         newgame.textContent="Play Again?";
         colorchanges(pickedcolor);
         head1.style.backgroundColor=this.style.backgroundColor;
       }else{
-        result.textContent="wrong";
+        result.src="cross.png";
         this.style.backgroundColor="#232323";
       }
     })
   }
-  
-
 
 function colorchanges(color){
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor=color;
     // head.style.backgroundColor=pickedcolor;
-
   }
 }
 
@@ -62,32 +52,52 @@ function rgb(){
   return ans;
 }
 
-newgame.addEventListener("click",function(){
-  // alert("new game start");
-  // location.reload();
+newgame.addEventListener("click", function(){
+// function hardl(){
+  easylevel.classList.remove("selected");
+  hardlevel.classList.add("selected");
+  result.src="finish-flag.png";
   head1.style.backgroundColor="rgb(97, 168, 230)";
   colors = ganaratcolor(6);
   pickedcolor=colors[Math.floor(Math.random() * colors.length)];
   for (let i = 0; i < squares.length; i++) {
+    squares[i].style.display="block";  
+    squares[i].style.backgroundColor=colors[i];    
+  }
+  colordispla.innerText=pickedcolor;
+})
+
+hardlevel.addEventListener("click",function(){
+  hardlevel.classList.add("selected");
+  easylevel.classList.remove("selected");
+  result.src="finish-flag.png";
+  head1.style.backgroundColor="rgb(97, 168, 230)";
+  colors = ganaratcolor(6);
+  pickedcolor=colors[Math.floor(Math.random() * colors.length)];
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.display="inline-block";  
     squares[i].style.backgroundColor=colors[i];    
   }
   colordispla.innerText=pickedcolor;
 });
 
-hardlevel.addEventListener("click",function(){
-  easylevel.classList.add(".selected");
-});
-
 easylevel.addEventListener("click",function(){
+  easylevel.classList.add("selected");
+  hardlevel.classList.remove("selected");
+  result.src="finish-flag.png";
+  head1.style.backgroundColor="rgb(97, 168, 230)";
   colors = ganaratcolor(3);
-  easylevel.classList.add(".selected");
   pickedcolor=colors[Math.floor(Math.random() * colors.length)];
   for (let i = 0; i < squares.length ; i++) {
     if(colors[i]){
       squares[i].style.backgroundColor=colors[i];  
     }else{
-      squares[i].style.display=none;  
+      squares[i].style.display="none";  
     } 
   }
   colordispla.innerText=pickedcolor;
+
+
+  // newgame.addEventListener("click", easyl);
 })
+
